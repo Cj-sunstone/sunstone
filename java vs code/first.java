b47c172d1b29d57eee9984bbce966ef678c89a4b
chirayu jain <chirayujain362005@gmail.com>
2024-04-30 12:37:02 +0530

    first commit

---
+++ java vs code/first.java
@@ -0,0 +1,12 @@
+public class first {
+    public static void main(String[] args) {
+        String x ="welcome to java class";
+        int y=28;
+        System.out.println(x);
+        System.out.println(y);
+        y=y+30;
+        System.out.println(y);
+
+    }
+}
+
